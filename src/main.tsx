3274a0ec1b9d57c859edeea6c3913f834ac91881
Andressa Mendes <54191888+andressamendes@users.noreply.github.com>
2026-01-02 19:10:52 -0300

    fix: adiciona BrowserRouter com basename para corrigir roteamento no GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import './styles/tutorial.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/medprompts">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
