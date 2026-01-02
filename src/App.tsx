7dfd0c368284a0fecee089456e57bc40be3020d3
Andressa Mendes <54191888+andressamendes@users.noreply.github.com>
2026-01-02 19:58:02 -0300

    feat: implementa sistema de error tracking com Error Boundaries e logging estruturado

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,23 @@ import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { FavoritesProvider } from './contexts/FavoritesContext';
 import { SkipLinks } from './components/SkipLinks';
+import { ErrorBoundary } from './components/ErrorBoundary';
 import Index from './pages/Index';
 
 function App() {
   return (
-    <ThemeProvider>
-      <FavoritesProvider>
-        <SkipLinks />
-        <div id="app-container">
-          <Routes>
-            <Route path="/" element={<Index />} />
-          </Routes>
-        </div>
-      </FavoritesProvider>
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <FavoritesProvider>
+          <SkipLinks />
+          <div id="app-container">
+            <Routes>
+              <Route path="/" element={<Index />} />
+            </Routes>
+          </div>
+        </FavoritesProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
 
